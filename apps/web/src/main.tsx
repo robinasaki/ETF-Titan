fe5fad6f153aadfea132ff8e357aed309702b698
Chenxu Robin Mao <robin@eden.so>
2026-04-14 18:43:43 -0400

    [tamagui] Getting rid of styles.css, centralize style def into tamagui

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { TamaguiProvider } from "tamagui";
 
+import { TamaguiProvider } from "tamagui";
 import { tamaguiConfig } from "@etf-titan/shared/theme.tamagui";
 
 import App from "./App";
-import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
